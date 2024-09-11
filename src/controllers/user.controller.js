b580917b10b6a63d20fb89133c8b3e8b14c9bc39
Ganesham Pareek <104836088+Ganesham09@users.noreply.github.com>
2024-09-11 19:04:38 +0530

    minor bug fixe

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -17,7 +17,7 @@ const registerUser = asyncHandler(async (req, res) => {
   // return res
 
   const { fullName, email, username, password } = req.body;
-  console.log('email:', email);
+  // console.log('email:', email);
   // if(fullName === ""){
   //   throw new ApiError(400, "fullname is required")
   // }
@@ -31,7 +31,7 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new ApiError(409, 'email seems to be not correct');
   }
 
-  const existedUser = User.findOne({
+  const existedUser = await User.findOne({
     $or: [{ username }, { email }],
   });
 
